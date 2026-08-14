1467b4296dff72df8519de9669960dc5f182c1cb
Wahab <lawalabdulwahab9@gmail.com>
2026-08-14 20:51:46 +0100

    feat: add device management components and API functions

---
+++ frontend/src/app/new/page.tsx
@@ -0,0 +1,4 @@
+// Placeholder: Add new device record form
+export default function NewDevice() {
+  return <div>Add New Device Form</div>;
+}
---
+++ frontend/src/lib/api.ts
@@ -0,0 +1,22 @@
+// Placeholder: API functions for device management
+// getDevices, getDevice, createDevice, updateDevice, deleteDevice
+
+export async function getDevices() {
+  // TODO: Implement
+}
+
+export async function getDevice(id: string) {
+  // TODO: Implement
+}
+
+export async function createDevice(data: unknown) {
+  // TODO: Implement
+}
+
+export async function updateDevice(id: string, data: unknown) {
+  // TODO: Implement
+}
+
+export async function deleteDevice(id: string) {
+  // TODO: Implement
+}
